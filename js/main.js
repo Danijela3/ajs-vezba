8d326a270c56974136326430c5a60435e17551c4
Danijela3 <danijela.trifkovic996@gmail.com>
2024-03-11 11:49:06 +0100

    izmene

--- js/main.js
+++ js/main.js
@@ -54,12 +54,15 @@
       speed: 300,
       slidesToShow: 1,
       slidesToScroll: 1,
-      nextArrow: '<span class="my-arrow my-arrow__next"><img src="image/arrows/arrow-right.png"></span>',
-      prevArrow: '<span class="my-arrow my-arrow__prev"><img src="image/arrows/arrow-left.png"></span>',
+      nextArrow: '<span class="my-arrow my-arrow__next"><img src="image/arrows/arrow-right.svg"></span>',
+      prevArrow: '<span class="my-arrow my-arrow__prev"><img src="image/arrows/arrow-left.svg"></span>',
       infinite: false
     }
   )
   });
+
+
+  /*OUR SPACES SLIDER*/
 
 
 
@@ -70,12 +73,42 @@
         speed: 300,
         slidesToShow: 1,
         slidesToScroll: 1,
-        nextArrow: '<span class="my-arrow my-arrow__next"><img src="image/arrows/arrow-right.png"></span>',
-        prevArrow: '<span class="my-arrow my-arrow__prev"><img src="image/arrows/arrow-left.png"></span>',
+        nextArrow: '<span class="my-arrow my-arrow__next"><img src="image/arrows/arrow-right.svg"></span>',
+        prevArrow: '<span class="my-arrow my-arrow__prev"><img src="image/arrows/arrow-left.svg"></span>',
         infinite: false
       }
     )
     });
+
+
+    /*OUR CLIENTS SLIDER*/
+
+    $(document).ready(function(){
+      $('.our-clients__grid').slick(
+        {
+          dots: true,
+          speed: 300,
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          nextArrow: '<span class="my-arrow my-arrow__next"><img src="image/arrows/arrow-right-grey.svg"></span>',
+          prevArrow: '<span class="my-arrow my-arrow__prev"><img src="image/arrows/arrow-left-grey.svg"></span>',
+          infinite: false,
+          responsive: [
+                   
+          {
+           breakpoint: 480,
+           settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+                        }
+              }
+                     
+                  ]
+        }
+      )
+      });
+
+
 
 
 
@@ -88,6 +121,25 @@ for (i=0; i<accordion.length; i++) {
     this.classList.toggle('active')
   })
 }
+
+
+
+
+
+
+
+
+
+// In your Javascript (external .js resource or <script> tag)
+
+// $(document).ready()
+// $(document).ready(function() {
+//   $('.interested-in').select2();
+// });
+
+
+
+
 
 
 
@@ -205,12 +257,71 @@ for (i=0; i<accordion.length; i++) {
 
 // HAMBURGER BUTTON----------------------------------------------------------------------------------------------//
 
-// const hamburgerBtn = document.getElementById("hamburger-button");
+const hamburgerBtn = document.getElementById("hamburger-button");
 
-// hamburgerBtn.addEventListener("click", () => {
-//   document.getElementById("hamburger-button").classList.toggle("change");
-//   document.getElementById("nav-links").classList.toggle("change");
-// } );
+let html = document.getElementsByTagName('html')[0];
+
+hamburgerBtn.addEventListener("click", () => {
+  
+  document.getElementById("navigation").classList.add("change");
+  html.classList.add("no-scroll");
+} );
+
+
+const closeBtn = document.getElementById("close-button");
+
+closeBtn.addEventListener("click", () => {
+  
+  document.getElementById("navigation").classList.remove("change");
+  html.classList.remove("no-scroll");
+} );
+
+
+
+
+
+
+const scrollListener = () => {
+  if (window.scrollY >= 10) {
+    hamburgerBtn.classList.add("transparent");
+    
+     
+ 
+   } else {
+     hamburgerBtn.classList.remove("transparent");
+    
+     
+   }
+};
+
+window.addEventListener('scroll', scrollListener);
+
+
+
+
+
+window.onscroll = function() {myFunction()};
+
+var stickyHeader = document.getElementById("sticky-header");
+
+
+function myFunction() {
+  if (window.scrollY >= 10) {
+    stickyHeader.classList.add("sticky");
+    
+
+  } else {
+    stickyHeader.classList.remove("sticky");
+    
+  }
+}
+
+
+
+
+
+
+   
 
 
 
